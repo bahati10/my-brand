64927c41a27f62fc464f32ebd832be157c4dc16e
Bahati <bahatiyves10@gmail.com>
2023-02-03 14:40:16 +0200

    Initiated Mongo with node server

---
+++ node-mongo-tutorial/mongo.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const routes = require("./routes")
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port =  process.env.PORT || 3000;
+
+app.use(express.json())
+app.use("/api", routes)
+     
+app.listen(port, () => {
+    console.log("Server is listening on port", port)
+})
+
+
+mongoose.set('strictQuery', false)
+    .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+    .then(() => {
+        console.log("connected to DB")
+    }).catch((err) => console.log("Error, couldn't connect to db", err))
+app.get("/", (req, res) => {
+    res.status(200).json({msg:"Welcome home"})
+})
